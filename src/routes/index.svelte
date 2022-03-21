16dbae6ac53f6510e74e6fd1c72e431942a33a68
Joshhortt <joshhortt@yahoo.com>
2022-03-21 18:03:07 +0000

    add  scripts to index.svelte'.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,25 @@
+<script context="module">
+export const load = async ({fetch}) => {
+  const res = await fetch('/posts.json');
+  if (res.ok) {
+    const {posts} = await res.json()
+    return  {
+      props: {posts},
+    }
+  }
+}
+</script>
+
+<script>
+import { stringify } from "postcss";
+
+  export let posts = []
+</script>
+
+
+<!-- This line of code below is to show posts in Json format in the browser -->
+<!-- <pre>{JSON.stringify(posts, null, 2)}</pre> --> 
+
 <h1>Welcome to SvelteKit</h1>
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
